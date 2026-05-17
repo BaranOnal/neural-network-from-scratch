8876c4184e482d6d3f0ebd59716c0000d32909d3
BaranOnal <baranonal301@gmail.com>
2026-05-17 16:46:51 +0300

    fix(layers): add missing shape print for the final layer

--- nn/layers.py
+++ nn/layers.py
@@ -1,8 +1,8 @@
 import numpy as np
-import nn.utils as utils
-import nn.activations as activations
-import nn.losses as losses
-import nn.optimizers as optimizers
+from . import utils
+from . import activations
+from . import losses
+from . import optimizers
 
 class NeuralNetwork:
 
@@ -29,6 +29,7 @@ class NeuralNetwork:
             self.layers.append(activation_class())
 
         self.layers.append(Dense(layers_dims[-2], layers_dims[-1], initialization=initialization))
+        print(f"Layer {len(layers_dims) - 1} | Weight Shape: ({layers_dims[-1]}, {layers_dims[-2]}) | (Out, In)")
 
         if loss_function == "categorical_crossentropy":
             self.layers.append(activations.Softmax())
@@ -146,39 +147,3 @@ def plot_training(history):
     plt.ylabel('Cost')
     plt.grid(True)
     plt.show()
-
-if __name__ == "__main__":
-    n_x = 5
-    m = 1000
-    X = np.random.randn(n_x, m)
-
-    # If the sum of its first two > 0, its 1; else, its 0.
-    Y = ((X[0, :] + X[1, :]) > 0).astype(float).reshape(1, m)
-
-    layers_dims = [5, 8, 1]
-
-    print("--- Model Running ---")
-    model = NeuralNetwork(X, Y, layers_dims,
-                          activation_function="relu",
-                          loss_function="binary_crossentropy",
-                          learning_rate=0.001,
-                          optimizer="Adam",
-                          initialization="he"
-                          )
-
-    model.fit(epochs=1000, batch_size=32)
-
-
-    print("\n--- Test Result ---")
-    test_pred = model.forward(model.x_test)
-
-    # Threshold = 0.5
-    pred = (test_pred > 0.5).astype(int)
-    acc = np.mean(pred == model.y_test)
-
-    print(f"Test example: {model.x_test.shape[1]}")
-    print(f"Total Cost: {model.loss_function.compute(model.y_test, test_pred):.5f}")
-    print(f"Accuracy: %{acc * 100:.2f}")
-
-
-    plot_training(model.history)
